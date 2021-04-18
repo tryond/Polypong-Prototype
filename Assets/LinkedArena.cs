f5b3b3f5bd86e62eef6d1fd23f27f5e87088d379
Danny Tryon <tryon.dan@gmail.com>
2021-04-17 23:18:51 -0700

    Clean up linked arena script

--- Assets/LinkedArena.cs
+++ Assets/LinkedArena.cs
@@ -1,33 +1,26 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using JetBrains.Annotations;
-using MiscUtil.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UIElements;
 using Random = UnityEngine.Random;
 
 public class LinkedArena : MonoBehaviour
 {
     public float sideLength = 1f;
-    public float transitionTime = 1000f;
+    public float transitionTime = 1f;
     public ArenaNode arenaNodePrefab;
     
     private List<ArenaNode> _nodes;
+    
     private float _currentRadius;
     private float _targetRadius;
     private Vector2[] _targetNormals;
-    [CanBeNull] private Coroutine _transition;
     
+    [CanBeNull] private Coroutine _transition;
     
     private void Awake()
     {
-        // TODO: remove
-        Application.targetFrameRate = 60;
-
         _currentRadius = 0f;
         _targetRadius = 0f;
         
@@ -38,14 +31,10 @@ public class LinkedArena : MonoBehaviour
         _nodes.Add(Instantiate(arenaNodePrefab, transform.position, Quaternion.identity));
     }
 
-    // TODO: debug
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.R))
-        {
-            // reset the current scene
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
     }
     
     private void FixedUpdate()
@@ -70,12 +59,6 @@ public class LinkedArena : MonoBehaviour
     
     public void Split()
     {
-        // // TODO: don't hardcode
-        // var arenaNode = _nodes[0];
-        //
-        // // find node index of node to split
-        // var nodeIndex = _nodes.IndexOf(arenaNode);
-
         var nodeIndex = Random.Range(0, _nodes.Count);
         var arenaNode = _nodes[nodeIndex];
 
@@ -85,63 +68,58 @@ public class LinkedArena : MonoBehaviour
         
         // set new targets, making this side flat
         _targetRadius = Polygon.GetRadius(_nodes.Count, sideLength);
-        
-        var rotatedTargetNormals = Polygon.GetVertexNormals(_nodes.Count, arenaNode.transform.up);
+        _targetNormals = Polygon.GetVertexNormals(_nodes.Count, arenaNode.transform.up, nodeIndex);
 
-        _targetNormals = new Vector2[rotatedTargetNormals.Length];
-        for (int i = 0; i < rotatedTargetNormals.Length; i++)
-            _targetNormals[(nodeIndex + i) % _targetNormals.Length] = rotatedTargetNormals[i];
-        
         if (_transition != null)
             StopCoroutine(_transition);
         
         _transition = StartCoroutine(SplitRoutine());
     }
-    
-    /*
-     * TODO:
-     *
-     * Coroutines which are stopped should not be, and instead should be
-     * combined somehow
-     *
-     * 
-     */
-    
-    
 
     public IEnumerator SplitRoutine()
     {
-        float transition = 0f;
-        float elapsedTime = 0f;
+        float t = 0f;
+        float timeElapsed = 0f;
 
         Vector2[] startNormals = new Vector2[_nodes.Count];
         for (int i = 0; i < _nodes.Count; i++)
             startNormals[i] = _nodes[i].transform.up;
 
         var startRadius = _currentRadius;
-        while (transition < 1f)
+        while (t < 1f)
         {
-            var t = elapsedTime / transitionTime;
-            // transition = Mathf.Clamp(1 - (1 - t) * (1 - t) * (1 - t), 0f, 1f);  // smooth stop
-            transition = Mathf.Clamp(t, 0f, 1f);
-            
+            t = SmoothStop(timeElapsed);
             var currentRadius = Mathf.Lerp(startRadius, _targetRadius, t);
             
-            // TODO: this should move based on rotation and radius -- not position !!!
+            // update node transforms
             for (int i = 0; i < _nodes.Count; i++)
             {
-                // TODO: this normal LERP is direction agnostic
-                // TODO: it should always go in the direction that makes it between its parents
+                /*
+                 * TODO:
+                 *
+                 * Because this directly sets the node's up vector, any concurrent
+                 * coroutine calls will overwrite each other.
+                 *
+                 * This is manageable for now, but clunky. A proper solution would
+                 * combine rotations from all concurrent coroutines and apply
+                 * those rotations on FixedUpdate.
+                 */
+                
                 _nodes[i].transform.up = Vector2.Lerp(startNormals[i], _targetNormals[i], t);
-
                 _nodes[i].transform.position = (currentRadius * _nodes[i].transform.up) + transform.position;
             }
         
             // wait for the end of frame and yield
             _currentRadius = currentRadius;
-            elapsedTime += Time.deltaTime;
+            timeElapsed += Time.deltaTime;
             yield return new WaitForEndOfFrame();
         }
         _transition = null;
+    }
+
+    private float SmoothStop(float timeElapsed)
+    {
+        var t = timeElapsed / transitionTime;
+        return Mathf.Clamp(1 - (1 - t) * (1 - t) * (1 - t), 0f, 1f);
     }
 }
